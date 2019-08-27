821c862d88e70209b4607b51f1c7f3b454616cc1
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-08-26 22:27:15 -0700

    automated

--- assets/javascript/projectUpdater.js
+++ assets/javascript/projectUpdater.js
@@ -1,22 +1,37 @@
-let repos;
+let repos = [];
 let htmlStuff;
 $.ajax({
     url: "https://api.github.com/users/Mercyfulsin/repos",
     method: 'GET'
-}).done(function(reply){
-    console.log("reply",reply);
-    repos = reply;
+}).done(function (reply) {
+    reply.forEach(function (content) {
+        let raw = `https://raw.githubusercontent.com/Mercyfulsin/${content.name}/master/profile.txt`;
+        verifiedRepo(raw) ? console.log("True") : console.log("False");
+    });
 });
 
-$.ajax({
-    url: "https://raw.githubusercontent.com/Mercyfulsin/Mercyfulsin.github.io/master/index.html",
-    method: 'GET'
-}).done(function(reply){
-    htmlStuff = reply;
-    addContent();
-});
+function verifiedRepo(site) {
+    $.ajax({
+        url: site,
+        method: 'GET'
+    }).done(function (reply) {
+        return true;
+    });
+}
 
-function addContent(){
-    console.log($("#Word-Guess-Game").html());
-    console.log(htmlStuff)
+// $.ajax({
+//     url: "https://raw.githubusercontent.com/Mercyfulsin/Word-Guess-Game/master/profile.txt",
+//     method: 'GET',
+//     statusCode: {
+//         404: function () {
+//             alert("page not found");
+//         }
+//     }
+// }).done(function (reply) {
+//     htmlStuff = reply;
+//     addContent();
+// });
+
+function addContent() {
+    $("#Word-Guess-Game").html(htmlStuff);
 }
