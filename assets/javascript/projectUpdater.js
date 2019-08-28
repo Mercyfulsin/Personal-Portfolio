8c040a7915dd32185664c6f5dbb14da8a82eb01b
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-08-27 19:55:51 -0700

    automated

--- assets/javascript/projectUpdater.js
+++ assets/javascript/projectUpdater.js
@@ -1,28 +1,48 @@
 let repos = [];
 let htmlStuff;
+
 $.ajax({
     url: "https://api.github.com/users/Mercyfulsin/repos",
     method: 'GET'
-}).done(function (reply) {
+}).then(function (reply) {
     let count = 0;
     reply.forEach(function (content) {
-        let raw = `https://raw.githubusercontent.com/Mercyfulsin/${content.name}/master/profile.txt`;
-        verifiedRepo(raw, content, count);
-        console.log("Checking " + count);
-        count++
+        let rawText = `https://raw.githubusercontent.com/Mercyfulsin/${content.name}/master/profile.txt`;
+        let rawImage = `https://raw.githubusercontent.com/Mercyfulsin/${content.name}/master/profile.png`;
+        verifiedRepo(rawText, rawImage, content, count, reply.length - 1);
+        console.log(count, reply.length);
+        count++;
     });
 });
 
-function verifiedRepo(site, obj, cnt) {
+function attach() {
+    console.log("attached");
+    var elems = document.querySelectorAll('.carousel');
+    var instance = M.Carousel.init(elems, {
+        fullWidth: true
+    });
+}
+
+function verifiedRepo(site, image, obj, curr, max) {
     $.ajax({
         url: site,
         method: 'GET'
     }).done(function (reply) {
         repos.push(obj);
-        addContent(reply, obj);
+        addContent(reply, image, obj);
+        if (curr === max)
     });
 }
 
-function addContent(html, obj) {
-    $("#" + obj.name).html(html);
+function addContent(html, image, obj) {
+    let container = $("#carousel-slides");
+    console.log(container);
+    let newSlide = $(`<div style="background-image: url('${image}'); background-position: center center; z-index: 0; opacity: 1; visibility: visible; transform: translateX(0px) translateX(0px) translateX(0px) translateZ(0px);" class="carousel-item white-text">`);
+    console.log(newSlide);
+    let slideText = $(`<div id="${obj.name}" style="background-color: rgb(114, 196, 196,0.5)">`);
+    slideText.html(html);
+    console.log(slideText);
+    console.log(slideText);
+    newSlide.append(slideText);
+    container.prepend(newSlide);
 }
